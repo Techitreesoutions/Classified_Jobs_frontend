098e27f575f96340f0a5f4bbf4cdbba9baa0f5f3
Usha <u.t.wadhwani@gmail.com>
2019-08-21 14:48:20 +0530

    2 files added for location

---
+++ src/actions/LocationListAction.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import locationList from "../data/cityList.json";
+//HC imports
+import {
+  createPlatformURL,
+  handleSecureAjaxError
+} from "../util/SecurePlatformAPIUtils";
+
+/** Action Types */
+export const GET_LOCATION_LIST = "get-location-list";
+
+/**
+ * Load Service List
+ */
+export const loadLocationList = callback => {
+  const url = createPlatformURL("config/locations ");
+  return dispatch => {
+    //dispatch(startLoading());
+    axios
+      .get(url)
+      .then(res => {
+        callback && callback();
+        console.log("Hello world from succes");
+        dispatch(getLocationList(res.data));
+      })
+      .catch(error => {
+        console.log("Hello world from failure");
+        handleSecureAjaxError(error, "ServiceActions loadService");
+        dispatch(getLocationList(locationList));
+        callback && callback();
+        //dispatch(stopLoading());
+      });
+  };
+};
+
+export const getLocationList = locationList => {
+  return {
+    type: GET_LOCATION_LIST,
+    payload: locationList
+  };
+};
---
+++ src/reducers/LocationListReducer.js
@@ -0,0 +1,21 @@
+import { GET_LOCATION_LIST } from "../actions/LocationListAction";
+
+//import { START_LOADING, STOP_LOADING } from "../actions/LoadingActions";
+
+export const CATAGORY_INITIAL_STATE = {
+  loading: false,
+  skillList: []
+};
+
+export default (state = CATAGORY_INITIAL_STATE, action) => {
+  switch (action.type) {
+    case GET_LOCATION_LIST:
+      return {
+        ...state,
+        locationList: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
